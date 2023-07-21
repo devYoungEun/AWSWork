438c5e6768ce602b0cd02fc7a6fa1da54765c986
서영은 <sye715@naver.com>
2023-07-21 17:46:58 +0900

    Day03

--- src/main/java/com/mega/it/springboot/service/posts/PostsService.java
+++ src/main/java/com/mega/it/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.mega.it.springboot.service.posts;
 
 import com.mega.it.springboot.domain.posts.Posts;
 import com.mega.it.springboot.domain.posts.PostsRepository;
+import com.mega.it.springboot.web.dto.PostsListResponseDto;
 import com.mega.it.springboot.web.dto.PostsResponseDto;
 import com.mega.it.springboot.web.dto.PostsSaveRequestDto;
 import com.mega.it.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. if=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/com/mega/it/springboot/web/IndexController.java
@@ -0,0 +1,27 @@
+package com.mega.it.springboot.web;
+
+import com.mega.it.springboot.service.posts.PostsService;
+import com.mega.it.springboot.web.dto.PostsListResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping(value = "/")
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+}
