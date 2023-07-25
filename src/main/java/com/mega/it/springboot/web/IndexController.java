2dff02a3b616450dac35ac20817377386a874c47
서영은 <sye715@naver.com>
2023-07-25 17:41:17 +0900

    Day04

--- src/main/java/com/mega/it/springboot/web/IndexController.java
+++ src/main/java/com/mega/it/springboot/web/IndexController.java
@@ -1,12 +1,17 @@
 package com.mega.it.springboot.web;
 
+import com.mega.it.springboot.config.auth.dto.SessionUser;
 import com.mega.it.springboot.service.posts.PostsService;
 import com.mega.it.springboot.web.dto.PostsListResponseDto;
+import com.mega.it.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,9 +19,15 @@ public class IndexController {
 
     private final PostsService postsService;
 
+    private final HttpSession httpSession;
+
     @GetMapping(value = "/")
     public String index(Model model){
         model.addAttribute("posts",postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName1",user.getName());
+        }
         return "index";
     }
 
@@ -24,4 +35,13 @@ public class IndexController {
     public String postsSave() {
         return "posts-save";
     }
+
+    @GetMapping("/posts/update/{id}")
+    public String postUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+        return "posts-update";
+    }
+
+
 }
--- src/main/java/com/mega/it/springboot/web/PostsApiController.java
+++ src/main/java/com/mega/it/springboot/web/PostsApiController.java
@@ -1,6 +1,7 @@
 package com.mega.it.springboot.web;
 
 import com.mega.it.springboot.service.posts.PostsService;
+import com.mega.it.springboot.web.dto.PostsResponseDto;
 import com.mega.it.springboot.web.dto.PostsSaveRequestDto;
 import com.mega.it.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -21,5 +22,14 @@ public class PostsApiController {
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
         return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) { return postsService.findById(id); }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
     }
 }
